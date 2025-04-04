3a4b9e6b60170ae7fc00617f2eda20065c37eb26
vadimko4 <vad.informat@gmail.com>
2025-04-04 07:46:35 +0400

    add fraction lesson3

--- lesson3/fraction.py
+++ lesson3/fraction.py
@@ -10,7 +10,32 @@
 
 
 class Fraction:
-    pass
+
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.numerator}, {self.denominator})"
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        min_denominator = min(self.denominator, other.denominator)
+        if min_denominator == self.denominator:
+            less = 'self'
+        else:
+            less = 'other'
+        for i in range(min_denominator, 0, -1):
+            if self.denominator % i == 0 and other.denominator % i == 0:
+                common_mult = i
+                break
+        self_mult = self.denominator // common_mult
+        other_mult = other.denominator // common_mult
+        lowest_common_denominator = common_mult * self_mult * other_mult
+        result_numerator = self.numerator * other_mult + other.numerator * self_mult
+        return f"{result_numerator}/{lowest_common_denominator}"
 
 
 # код для проверки 
