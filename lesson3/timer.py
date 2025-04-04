39da15075e50924937f6fcdf585273b2b3f4f7bc
vadimko4 <vad.informat@gmail.com>
2025-04-04 23:36:35 +0400

    add timer lesson3

--- lesson3/timer.py
+++ lesson3/timer.py
@@ -1,3 +1,5 @@
+import time
+
 """
 Напишите класс Timer, который будет вычислять время выполнения блока кода. Класс должен иметь следующие методы:
 
@@ -8,11 +10,21 @@
 
 
 class Timer:
-    pass
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.time()
+        # print(f"Время выполнения: {self.end - self.start} секунд")
+        self.elapsed_time = self.end - self.start
+        return self
 
 
 with Timer() as timer:
     # блок кода
-    
+    for i in range(10 ** 8):
+        flag = True
     # код для проверки 
-    print("Execution time:", timer.elapsed_time)
+print("Execution time:", timer.elapsed_time)
